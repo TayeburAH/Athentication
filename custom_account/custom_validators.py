1b8f9d0da3741a9868cff9f9d9793b60ca7ff8da
TayeburAH <tayebur687@gmail.com>
2021-06-20 11:12:21 +0600

    added

---
+++ custom_account/custom_validators.py
@@ -0,0 +1,19 @@
+from django.core.validators import ValidationError
+from validate_email import validate_email
+import re
+import DNS
+
+DNS.defaults['server']=['8.8.8.8', '8.8.4.4']
+def validate_phone_number_or_email(email_phone):
+    if email_phone.isnumeric():
+        phone = email_phone
+        if not re.search(r"^[1][1739685]\d{8}", phone):
+            raise ValidationError('Invalid phone number')
+        else:
+            return phone
+    else:
+        email = email_phone
+        if not validate_email(email, verify=True):
+            raise ValidationError('Invalid Email')
+        else:
+            return email
---
+++ custom_account/signals.py
@@ -0,0 +1,105 @@
+import shutil
+import os
+
+from django.db import transaction
+from django.db.models.signals import pre_save, post_save, post_delete, pre_delete
+from django.dispatch import receiver
+from allauth.account.admin import EmailAddress
+from django.conf import settings
+from django.contrib import messages
+from django.shortcuts import redirect
+from .models import *
+from django.core.files.storage import default_storage as storage
+import cloudinary.uploader
+
+User = settings.AUTH_USER_MODEL
+import cloudinary
+from allauth.account.signals import user_signed_up
+
+
+# Usually pre_delete is used to clean up, the folder after delete() is clicked
+
+
+@receiver(pre_delete, sender=User)  # post_delete : you have to delete twice
+# as the instance still exist after the save()
+def delete_folder(sender, instance, *args, **kwargs):
+    try:
+        cloudinary.api.delete_resources_by_prefix(f"media/profile_images/{instance.pk}")
+        print('Deleted all image')
+        cloudinary.api.delete_folder(f"media/profile_images/{instance.pk}")  # Folder not empty
+        print(f'Deleted folder {instance.pk}')
+        # default_storage.delete(instance.qr_image.name)
+        # path = os.path.join(os.getcwd(), f'media\profile_images\{instance.pk}')
+        # shutil.rmtree(path)
+    except Exception as e:
+        print(e)
+
+
+# @receiver(post_save, sender=User)  # post_delete : you have to delete twice
+# # as the instance still exist after the save()
+# def resize_pic(sender, instance, *args, **kwargs):
+#     # user = User.objects.get(id=instance.id)
+#     # cloudinary.api.delete_resources([user.profile_image])
+#     try:
+#         img = Image.open(instance.profile_image.name) # too fast
+#         print(instance.profile_image.name)
+#         if img.height > 300 or img.width > 300:
+#             output_size = (300, 300)
+#             img.thumbnail(output_size)
+#             img.save(instance.profile_image.name)
+#     except Exception as e:
+#         print(e)
+
+
+# pre_save.connect(delete_folder, sender=settings.AUTH_USER_MODEL)
+# If you want to register the receiver function to several signals you may do it like this:
+# @receiver([post_save, post_delete], sender=User)
+# signal does not work when using it in a separate file
+
+# When account is created via social, fire django-allauth signal to populate Django User record.
+@receiver(user_signed_up)
+@transaction.atomic
+def populate_profile(request, sociallogin, user, **kwargs):
+    """Signal, that gets extra data from sociallogin and put it to profile."""
+
+    if sociallogin.account.provider == 'facebook':
+        user_data = user.socialaccount_set.filter(provider='facebook')[0].extra_data
+        email = user_data['email'].lower()
+        first_name = user_data['first_name']
+        last_name = user_data['last_name']
+
+    if sociallogin.account.provider == 'google':
+        user_data = user.socialaccount_set.filter(provider='google')[0].extra_data
+        email = user_data['email']
+
+        first_name = " ".join(user_data['name'].split(' ')[0:-1])
+        last_name = user_data['name'].split(' ')[-1]
+
+    user.email_phone = user.email = email  # saves in both User, Social Account, User account
+    user.is_customer = True
+    user.save()
+
+    # Email verification is auto with google but not for facebook
+    # so we need to check our user is verifed or not
+    if user.emailaddress_set.filter(verified=False).exists():
+        account = EmailAddress.objects.get(email=user.email, verified=False)
+        account.verified = True
+        account.save()
+    # now make a customer dor the corresponding user
+    customer = Customer.objects.create(user=user)
+    # student.attributes=self.cleaned_data.get(' attributes ')
+    # Form must have this new attributes forms.charfield
+    customer.first_name = first_name
+    customer.last_name = last_name
+    customer.save()
+    return customer
+
+    '''
+    To access any instance data sociallogin
+    sociallogin.account.extra_data['picture']
+    sociallogin.account.uid
+    sociallogin.account.provider
+    sociallogin == socialacount table
+    etc
+    
+    '''
